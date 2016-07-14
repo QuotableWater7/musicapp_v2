226e5468785d6905146d78a1b7e17773e1bc9ad0
JT <jbowler2400@gmail.com>
2016-07-14 07:22:37 -0700

    Fix the layout component to use bootstrap

--- client/app/layout.js
+++ client/app/layout.js
@@ -17,11 +17,13 @@ class Layout extends React.Component {
             </div>
           </div>
         </div>
-        <div className='row'>
-          <div className='col-lg-12'>
-            <p>This is the layout component!!!</p>
-            <Link to='/add'>Add</Link>
-            {this.props.children}
+        <div className='container'>
+          <div className='row'>
+            <div className='col-lg-12'>
+              <p>This is the layout component!!!</p>
+              <Link to='/add'>Add</Link>
+              {this.props.children}
+            </div>
           </div>
         </div>
       </div>
