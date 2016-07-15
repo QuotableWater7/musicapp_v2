f0d77ef8ee8cc3720672479eba62bd110785b0da
JT <jbowler2400@gmail.com>
2016-07-15 15:35:42 -0700

    Remove some logic that wasn't necessary

--- client/app/components/setup.js
+++ client/app/components/setup.js
@@ -1,27 +1,14 @@
 'use strict';
 
 import React from 'react';
-import { connect } from 'react-redux';
 import DrillsTable from './drills_table';
 
 const Setup = ({ drills }) => (
   <div className='row'>
     <div className='col-md-8 col-md-offset-2'>
-      <DrillsTable drills={drills}/>
+      <DrillsTable/>
     </div>
   </div>
 )
 
-const mapStateToProps = (state) => {
-  return {state};
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    removeDrill(id) {  },
-  };
-}
-
-const wrapper = connect(mapStateToProps, mapDispatchToProps)(Setup);
-
-export default wrapper;
+export default Setup;
