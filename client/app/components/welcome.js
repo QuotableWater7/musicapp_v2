39ab691a3ee81e5ea1c31330abb2dd700ca709a2
JT <jbowler2400@gmail.com>
2016-07-18 09:01:56 -0700

    Add more text to the welcome screen

--- client/app/components/welcome.js
+++ client/app/components/welcome.js
@@ -5,8 +5,16 @@ import React from 'react';
 const Welcome = () => {
   return (
     <div className='row'>
-      <div className='col-md-12'>
+      <div className='col-md-8 col-md-offset-2'>
         <h3 className='text-xs-center'>Welcome!</h3>
+        <hr/>
+        <p>
+          This tool helps musicians organize their practice time in a more optimal
+          way. It allows you to select your total available time and a list of
+          activities to practice. After weighting the activities by importance, the
+          time is divied up and you can practice without having to worry about when
+          to change focus!
+        </p>
       </div>
     </div>
   );
