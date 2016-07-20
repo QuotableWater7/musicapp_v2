c8fb53f9b5d50c3c857942c19b1576e4ad8f235c
JT <jbowler2400@gmail.com>
2016-07-20 09:47:47 -0700

    Use hashHistory for now to avoid refresh issues with rails

--- client/app/index.js
+++ client/app/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { combineReducers } from 'redux-immutable';
 
@@ -18,7 +18,7 @@ import reducers from './reducers/index';
 import timerUpdater from './util/timer_updater';
 
 const store = createStore(combineReducers(reducers));
-const history = syncHistoryWithStore(browserHistory, store, {
+const history = syncHistoryWithStore(hashHistory, store, {
   selectLocationState (state) {
     return state.get('routing').toJS();
   }
