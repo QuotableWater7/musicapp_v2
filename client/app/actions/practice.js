827ca777965edb2b3ce6d9ea2596150d3d738539
JT <jbowler2400@gmail.com>
2016-07-24 14:51:53 -0700

    Finish replacing isomorphic-fetch with axios + my helpers

--- client/app/actions/practice.js
+++ client/app/actions/practice.js
@@ -1,7 +1,6 @@
 'use strict';
 
-require('es6-promise').polyfill();
-require('isomorphic-fetch');
+import { get } from '../util/ajax_helper';
 
 export const UPDATE_TOTAL_TIME = 'UPDATE_TOTAL_TIME';
 export const RESET_TIMER = 'RESET_TIMER';
@@ -9,12 +8,8 @@ export const TIMER_COMPLETED = 'TIMER_COMPLETED';
 
 export const fetchPractice = () => {
   return (dispatch) => {
-    fetch('/practices/5.json').then(
-      (response) => {
-        response.json().then(
-          (json) => dispatch({ type: 'LOAD_DRILLS', payload: json })
-        )
-      }
-    );
+    get('/practices/5.json').then((response) => {
+      dispatch({ type: 'LOAD_DRILLS', payload: response.data })
+    });
   };
 };
