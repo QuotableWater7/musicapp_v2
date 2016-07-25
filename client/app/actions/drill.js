0e05717a67bfa5e56902af1cb4b5c4298bb45cba
JT <jbowler2400@gmail.com>
2016-07-25 08:27:58 -0700

    Add basic actions for drills

---
+++ client/app/actions/drill.js
@@ -0,0 +1,5 @@
+'use strict';
+
+export const FETCH_DRILLS = 'FETCH_DRILLS';
+export const FETCH_DRILLS_SUCCESS = 'FETCH_DRILLS_SUCCESS';
+export const FETCH_DRILLS_FAILURE = 'FETCH_DRILLS_FAILURE';
