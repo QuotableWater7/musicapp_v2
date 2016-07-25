2e6fb37d1de45bbdc34d6192ee64db1e57ae1ce8
JT <jbowler2400@gmail.com>
2016-07-25 09:01:37 -0700

    Add success/failure actions to practice reducer

--- client/app/reducers/practice.js
+++ client/app/reducers/practice.js
@@ -5,7 +5,10 @@ import { Map } from 'immutable';
 import {
   UPDATE_TOTAL_TIME,
   RESET_TIMER,
-  TIMER_COMPLETED
+  TIMER_COMPLETED,
+  FETCH_PRACTICE,
+  FETCH_PRACTICE_SUCCESS,
+  FETCH_PRACTICE_FAILURE,
 } from '../actions/practice';
 
 const default_state = Map({
@@ -21,6 +24,10 @@ export default (state = default_state, action) => {
     return state.merge({ current_drill_index: action.current_drill_index })
   case TIMER_COMPLETED:
     return state.merge({ current_drill_index : state.get('current_drill_index') + 1 });
+  case FETCH_PRACTICE_SUCCESS:
+    return state.merge(action.payload);
+  case FETCH_PRACTICE_FAILURE:
+    return default_state.merge({});
   }
 
   return state;
