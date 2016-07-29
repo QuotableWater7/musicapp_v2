93fa871c77893b60f50e2e56b49fe205dd689b3a
JT <jbowler2400@gmail.com>
2016-07-28 17:31:44 -0700

    Prep the practice list component for more advanced display

--- client/app/components/practice_list.js
+++ client/app/components/practice_list.js
@@ -3,12 +3,24 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-export default () => {
+const PracticeList = () => {
   return (
-    <div className='row'>
-      <div className='col-md-8 col-md-offset-4'>
-        Loading...
+    <div>
+      <div className='row'>
+        <div className='col-md-8 col-md-offset-4'>
+          <div className='btn btn-primary'>
+            New Practice
+          </div>
+        </div>
+      </div>
+      <div className='row margin-top-10'>
+        <div className='col-md-8 col-md-offset-4'>
+          <h5>Existing Practices</h5>
+          ...
+        </div>
       </div>
     </div>
   );
 };
+
+export default PracticeList;
