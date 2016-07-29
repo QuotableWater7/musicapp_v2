5eb3d8702ef56e321cc1e92f67ceb364ce21c684
JT <jbowler2400@gmail.com>
2016-07-29 09:41:40 -0700

    Add another controller test

--- spec/controllers/practices_controller_spec.rb
+++ spec/controllers/practices_controller_spec.rb
@@ -3,15 +3,19 @@ RSpec.describe PracticesController do
   describe '#index' do
     let(:user) { users(:user_1) }
 
-    before(:each) do
-      login_as(user)
-    end
-
     it 'returns a list of practices associated with the current user' do
+      login_as(user)
       user.practices.create!(name: 'bloop', total_time: 120)
       get(:index,  format: :json)
 
       expect(JSON.parse(response.body)).to eq [user.practices.first.as_json]
+    end
+
+    it 'returns empty array when user not logged in' do
+      user.practices.create!(name: 'bloop', total_time: 120)
+
+      get(:index,  format: :json)
+      expect(JSON.parse(response.body)).to eq []
     end
   end
 
