532c24d9a6f12a3713cd175633eaf40c8baf42be
JT <jbowler2400@gmail.com>
2016-07-29 07:11:30 -0700

    Add another test for controller index method
    Also updates some important settings for the test database.

--- spec/controllers/practices_controller_spec.rb
+++ spec/controllers/practices_controller_spec.rb
@@ -1,9 +1,26 @@
 RSpec.describe PracticesController do
 
-  it 'creates a practice with the given name' do
-    post(:create, practice: { name: 'bloop' }, format: :json)
+  describe '#index' do
+    let(:user) { users(:user_1) }
 
-    expect(JSON.parse(response.body)['name']).to eq 'bloop'
+    before(:each) do
+      login_as(user)
+    end
+
+    it 'returns a list of practices associated with the current user' do
+      user.practices.create!(name: 'bloop', total_time: 120)
+      get(:index,  format: :json)
+
+      expect(JSON.parse(response.body)).to eq [user.practices.first.as_json]
+    end
+  end
+
+  describe '#create' do
+    it 'creates a practice with the given name' do
+      post(:create, practice: { name: 'bloop' }, format: :json)
+
+      expect(JSON.parse(response.body)['name']).to eq 'bloop'
+    end
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,13 @@ require 'rspec/rails'
 
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.global_fixtures = :all
+end
+
+def login_as(user)
+  remember_token = User.new_remember_token
+  cookies.permanent[:remember_token] = remember_token
+  user.update_attribute(:remember_token, User.digest(remember_token))
 end
