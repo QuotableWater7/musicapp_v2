13ae55fa53bfc7a3837d124ca04798e7fe4d63c0
JT <jbowler2400@gmail.com>
2016-07-28 17:44:20 -0700

    Add an empty state entry for practice ui stuff

--- client/app/reducers/index.js
+++ client/app/reducers/index.js
@@ -5,6 +5,7 @@ import { combineReducers } from 'redux-immutable';
 import drill_list from './drill_list';
 import routing from './routing';
 import practice from './practice';
+import practiceUI from './practice_ui';
 import timer from './timer';
 import user from './user';
 
@@ -13,5 +14,6 @@ export default combineReducers({
   drill_list,
   routing,
   practice,
+  practiceUI,
   timer
 })
---
+++ client/app/reducers/practice_ui.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import { Map } from 'immutable';
+
+const default_state = Map();
+
+export default (state = default_state, action) => {
+
+  return state;
+};
