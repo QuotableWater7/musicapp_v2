33202cf13f65f082a74409862c455825951a42b8
JT <jbowler2400@gmail.com>
2016-07-29 07:13:26 -0700

    Remove garbage action in practices controller

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -9,22 +9,6 @@ class PracticesController < ApplicationController
     end
   end
 
-  def show
-    respond_to do |format|
-      format.json do
-        current_id = 1
-        data = {
-          current_id => { title: 'Drill One', weight: 50, id: current_id },
-          (current_id += 1) => { title: 'Drill Two', weight: 50, id: current_id },
-          (current_id += 1) => { title: 'Drill Three', weight: 50, id: current_id },
-          (current_id += 1) => { title: 'Drill Four', weight: 50, id: current_id },
-          (current_id += 1) => { title: 'Drill Five', weight: 50, id: current_id }
-        }
-        render status: :ok, json: { drills: data }
-      end
-    end
-  end
-
   def create
     respond_to do |format|
       format.json do
