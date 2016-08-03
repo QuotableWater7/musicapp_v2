2aa86f7d1e7f4336b55f06decab61432f56f1da3
JT <jbowler2400@gmail.com>
2016-08-03 08:07:26 -0700

    Get rid of <br> tags in favor of css

--- client/app/components/layout.js
+++ client/app/components/layout.js
@@ -5,9 +5,7 @@ import Toolbar from './toolbar';
 const Layout = ({ children }) => {
   return (
     <div className='container'>
-      <br/>
       <Toolbar/>
-      <br/><br/>
       {children}
     </div>
   );
